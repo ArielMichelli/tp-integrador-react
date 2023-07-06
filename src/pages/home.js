2e2c9508f71d8aad90b6a5bf0e8c188dcedba8ae
Javier Gil <javiergonzalog22@gmail.com>
2023-07-05 23:46:13 -0300

    Update by Javier Gil
    -Se agrego codigo a las paginas de:
    Inicio, Series, Peliculas y MiLista para validar si el usuario esta logueado.
    
    Si el usuario no esta logueado e intenta ingresar a la URL sera redireccionado a la pagina principal.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,25 @@
-import { useContext } from 'react'
 import { MainContextState } from '@/components/context/MainContextProvider'
+import { useRouter } from 'next/router'
+import { useContext, useEffect } from 'react'
 
 const Home = () => {
-  const { state } = useContext(MainContextState)
+  const { state, dispatch } = useContext(MainContextState)
+  const router = useRouter()
 
+  const logOut = () => {
+    dispatch({ type: 'LOGOUT' })
+    if (typeof window !== 'undefined') {
+      router.push('/') // Redirige a la página principal después de cerrar sesión
+    }
+  }
+
+  const isLoggedIn = state.isLoggedIn
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      logOut() // Cierra sesión si el usuario no está logueado
+    }
+  }, [])
   const newLocal = 'm-0 p-2 text-center text-white bg-dark border rounded border-warning text-capitalize'
   return (
     <>
