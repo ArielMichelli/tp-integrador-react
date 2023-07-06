640639e9ce47f95a51d7bf1697ee683bb0334cb1
Javier Gil <javiergonzalog22@gmail.com>
2023-07-05 22:05:03 -0300

    Update by Javier Gil
    -Pasar StandardJavaScript a todos los archivos.
    -Al desloguearse envia a la pagina principal

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,52 +1,52 @@
-import { useContext, useEffect, useState } from "react";
-import { collection,doc, getDocs, addDoc, deleteDoc,  query, where } from "firebase/firestore";
-import {  db } from "@/firebase/initConfig"
-import { MainContextState } from "../context/MainContextProvider";
+import { useContext, useEffect, useState } from 'react'
+import { collection, doc, getDocs, addDoc, deleteDoc, query, where } from 'firebase/firestore'
+import { db } from '@/firebase/initConfig'
+import { MainContextState } from '../context/MainContextProvider'
 // Importe de estilos de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FetchData } from '../FetchData'
 import VisualCard from '../VisualCard'
 
-let dataFav=[]
+let dataFav = []
 
 const Favoritos = () => {
-  const { state  } = useContext(MainContextState)
- // const [data, setData]=useState()
+  const { state } = useContext(MainContextState)
+  // const [data, setData]=useState()
 
   const obtenerLista = () => {
-    dataFav=[]
-    const myCollection = collection(db, "listas")
-    const q1 = query(myCollection, where("UID", "==", state.userID));
+    dataFav = []
+    const myCollection = collection(db, 'listas')
+    const q1 = query(myCollection, where('UID', '==', state.userID))
     getDocs(q1).then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         // console.log(doc.id, ' => ', doc.data());
-       
+
         dataFav.push(doc.data())
-        //console.log(dataFav);
-      });
+        // console.log(dataFav);
+      })
     })
   }
-   
-  //obtenerLista()
-  useEffect(()=>{
-    obtenerLista()
-  },[]);
-    
-      // querySnapshot.forEach((mov) => (
-       // console.log(data)
-      //   ))
 
-        return (
-          <div className='container-fluid movies__container'>
-            {console.log("la data es:", dataFav)}
-            { 
+  // obtenerLista()
+  useEffect(() => {
+    obtenerLista()
+  }, [])
+
+  // querySnapshot.forEach((mov) => (
+  // console.log(data)
+  //   ))
+
+  return (
+    <div className='container-fluid movies__container'>
+      {console.log('la data es:', dataFav)}
+      {
               dataFav.forEach((mov) => (
               // console.log("mov: ",mov)
-                <VisualCard key={mov.id} {...mov } />
-            ))
+                <VisualCard key={mov.id} {...mov} />
+              ))
           }
-          </div>
-        )
-      }
+    </div>
+  )
+}
 
 export default Favoritos
--- src/pages/favoritos.js
+++ src/pages/favoritos.js
@@ -1,6 +1,6 @@
 // import {  useContext } from "react";
 // import { MainContextState } from "@/components/context/MainContextProvider";
-import Favorites from "@/components/Favorites/Favorites"
+import Favorites from '@/components/Favorites/Favorites'
 
 const Favoritos = () => {
   const newLocal = 'm-0 p-2 text-center text-white bg-dark border rounded border-danger text-capitalize'
