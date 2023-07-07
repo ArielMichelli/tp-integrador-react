e1ceaf387d82504075c969f702b4997fb8d8b4b4
miriam <73286285+arialcoder@users.noreply.github.com>
2023-07-06 22:00:56 -0300

    SearchBar, SearchList, buscar, FullNavbar

--- src/components/search/SearchList.jsx
+++ src/components/search/SearchList.jsx
@@ -4,8 +4,8 @@ export const SearchList = ({ movies }) => {
   // console.log(movies)
   return (
     <>
-      {
-        movies?.map(movie => (
+     {movies.length === 0 && <h3 className='text-light'> Pelicula no encontrada </h3>}
+      { movies?.map(movie => (
           <VisualCard key={movie.id} {...movie} />
         ))
       }
--- src/pages/buscar.js
+++ src/pages/buscar.js
@@ -1,40 +1,29 @@
 import { useState, useEffect } from 'react'
 import { SearchList } from '@/components/search/SearchList'
 import { getMovieByName } from '../components/search/getMovieByName'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function Buscar () {
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useRouter } from 'next/router'
+
+export default function Buscar (props) {
+
+  const router = useRouter()
+  const searchValue = router.query.searchValue;
   const [movies, setMovies] = useState([])
 
+
   useEffect(() => {
-    getMovieByName('marvel')
+    getMovieByName(searchValue)
       .then(movies => setMovies(movies))
     console.log(movies)
   },
-  [])
+  [searchValue])
 
   return (
     <>
       <h1 className='m-0 p-2 text-center text-white bg-dark'>Buscar película por nombre</h1>
-      {/* <div className='d-flex justify-content-center align-items-center bg-dark'>
-        <Form className='d-flex col col-sm-4' onSubmit={handlerSubmit}>
-          <Form.Control
-            type='search'
-            placeholder='Search'
-            className='me-auto'
-            aria-label='Search'
-            value={searchValue}
-             // onChange = {handlerOnChance}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
-          <Button style={{border:'none'}} variant="link" type='submit'><i className="bi bi-search text-light"></i></Button>
-        </Form>
-      </div> */}
       <main className='container-fluid  movies__container justify-center'>
         <SearchList key={movies.id} movies={movies} />
-
       </main>
     </>
   )
